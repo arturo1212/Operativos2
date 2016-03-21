1c0a254bc961d4e622de7fd3693a009a1527b13e
Elechero <davidhervil@hotmail.com>
2016-03-21 18:30:45 -0430

    Adicion de lectura de paramtros
    Falta acomodar lo de salida y NUM_Threads

--- UsoDisco.c
+++ UsoDisco.c
@@ -162,22 +162,183 @@ int main (int argc, char *argv[])
 
     /*Cosas para los threads*/
   	pthread_t threads[NUM_THREADS];
-  	int rc,i, listos = 0;
+  	int rc,i, listos = 0,index,cant_threads;
   	long t;
-  	char cwd[1024],palabra[1024];
+  	char cwd[1024],palabra[1024],*salida,op1,op2;
  	/*Asignar el tamanio del arreglo de mutex para el bloqueo*/
   	directorios = (char**)malloc(sizeof(char*)*NUM_THREADS);
   	/*Asignar el tamanio del arreglo de hilos libres*/
   	libre =(int*)malloc(sizeof(int)*NUM_THREADS);
- 
-/* Revisar los directorios del master para agregarlos a la cola*/
-	if (getcwd(cwd, sizeof(cwd)) != NULL){
-        fprintf(stdout, "Current working dir: %s\n", cwd);
+ 	if(argc==1){
+		if (getcwd(cwd, sizeof(cwd)) != NULL){
+	        fprintf(stdout, "Current working dir: %s\n", cwd);
+		}else{
+			perror("getcwd() error");
+			return 0;
+		}
+		salida="/dev/stdout";
+	}else{
+		if(argc==2){
+			if((strcmp(argv[1],"-h"))==0){
+				printf("UsoDisco  [-h] |  [-n i] [-d directorio] [-o salida ]\n");
+				printf("------------------------------\n");
+printf("-h 	muestra por pantalla un mensaje de ayuda (sintaxis, descripción de parámetros, etc.) y termina\n");
+printf("-n i 	nivel de concurrencia solicitado. Por defecto crea un solo hilo trabajador\n");
+printf("-d directorio 	especifica un directorio desde donde calcula el espacio utilizado.");
+printf(" Por defecto hace el cálculo desde el directorio actual.\n");
+printf("-o salida 	archivo que contendrá la salida con la lista de directorios y el espacio en bloques");
+printf(" ocupado por los archivos regulares. El valor por defecto es la salida estándar.\n");
+			}else{
+				fprintf(stderr, "Uso esperado : %s  [-h] |  [-n i] [-d directorio] [-o salida ]\n", argv[0]);
+				return 1;
+			}
+		}else{
+			if(argc==3){ //Falta anhadir valores por defecto
+				if(argv[1][0]=='-'){
+					if (argv[1][1]=='n'){
+
+						i=atoi(argv[2]);
+						if (getcwd(cwd, sizeof(cwd)) != NULL){
+					        fprintf(stdout, "Current working dir: %s\n", cwd);
+						}else{
+							perror("getcwd() error");
+							return 0;
+						}
+						salida="/dev/stdout";
+
+					}else if(argv[1][1]=='d'){
+						memcpy(cwd,argv[2],strlen(argv[2]));
+						cwd[strlen(cwd)] = '\0';
+						salida="/dev/stdout";
+
+					}else if(argv[1][1]=='o'){
+
+						salida = (char *)malloc(strlen(argv[2])+1);
+						memcpy(salida,argv[2],strlen(argv[2]));
+						salida[strlen(salida)] = '\0';
+						if (getcwd(cwd, sizeof(cwd)) != NULL){
+					        fprintf(stdout, "Current working dir: %s\n", cwd);
+						}else{
+							perror("getcwd() error");
+							return 0;
+						}
+
+					}else{
+						fprintf(stderr, "Uso esperado : %s  [-h] |  [-n i] [-d directorio] [-o salida ]\n", argv[0]);
+						return 1;
+					}
+				}else{
+					fprintf(stderr, "Uso esperado : %s  [-h] |  [-n i] [-d directorio] [-o salida ]\n", argv[0]);
+					return 1;
+				}
+			}else{
+				if(argc==4){
+					//Mensaje de error
+					fprintf(stderr, "Uso esperado : %s  [-h] |  [-n i] [-d directorio] [-o salida ]\n", argv[0]);
+					return 1;
+				}else{
+					if(argc==5){ //Falta anhadir valores por defecto
+						if(argv[1][0]=='-'){
+							op1=argv[1][1];
+
+							if (argv[1][1]=='n'){
+								cant_threads=atoi(argv[2]);
+							}else if(argv[1][1]=='d'){
+								memcpy(cwd,argv[2],strlen(argv[2]));
+								cwd[strlen(cwd)] = '\0';
+							}else if(argv[1][1]=='o'){
+								salida = (char *)malloc(strlen(argv[2])+1);
+								memcpy(salida,argv[2],strlen(argv[2]));
+								salida[strlen(salida)] = '\0';
+							}else{
+								fprintf(stderr, "Uso esperado : %s  [-h] |  [-n i] [-d directorio] [-o salida ]\n", argv[0]);
+								return 1;
+							}
+							if(argv[3][0]=='-'){
+								op2=argv[3][1];
+								if(argv[3][1]=='n'){
+									cant_threads=atoi(argv[4]);
+								}else if(argv[3][1]=='d'){
+									memcpy(cwd,argv[4],strlen(argv[4]));
+									cwd[strlen(cwd)] = '\0';
+								}else if(argv[3][1]=='o'){
+									salida = (char *)malloc(strlen(argv[4])+1);
+									memcpy(salida,argv[4],strlen(argv[4]));
+									salida[strlen(salida)] = '\0';
+								}else{
+									fprintf(stderr, "Uso esperado : %s  [-h] |  [-n i] [-d directorio] [-o salida ]\n", argv[0]);
+									return 1;
+								}
+								if(op1=='n'){
+									if(op2=='d'){
+										salida="/dev/stdout";
+									}else if(op2=='o'){
+										if (getcwd(cwd, sizeof(cwd)) != NULL){
+											fprintf(stdout, "Current working dir: %s\n", cwd);
+										}else{
+											perror("getcwd() error");
+											return 0;
+										}
+									}
+								}else if (op1=='d'){
+									if(op2=='n'){
+										salida="/dev/stdout";
+									}
+								}else if(op1=='o'){
+									if(op2=='n'){
+										if (getcwd(cwd, sizeof(cwd)) != NULL){
+											fprintf(stdout, "Current working dir: %s\n", cwd);
+										}else{
+											perror("getcwd() error");
+											return 0;
+										}
+									}
+								}
+							}else{
+								fprintf(stderr, "Uso esperado : %s  [-h] |  [-n i] [-d directorio] [-o salida ]\n", argv[0]);
+								return 1;
+							}
+						}else{
+							fprintf(stderr, "Uso esperado : %s  [-h] |  [-n i] [-d directorio] [-o salida ]\n", argv[0]);
+							return 1;
+						}
+					}else{
+						if(argc=6){
+							//Mensaje de error
+							fprintf(stderr, "Uso esperado : %s  [-h] |  [-n i] [-d directorio] [-o salida ]\n", argv[0]);
+							return 1;
+						}else if(argc>7){
+							//Mensaje de error
+							fprintf(stderr, "Uso esperado : %s  [-h] |  [-n i] [-d directorio] [-o salida ]\n", argv[0]);
+							return 1;
+						}else{
+							for(index = 1;index<7;index+=2){
+								if(argv[index][0]=='-'){
+									if (argv[index][1]=='n'){
+										cant_threads=atoi(argv[index+1]);
+									}else if(argv[index][1]=='d'){
+										memcpy(cwd,argv[index+1],strlen(argv[index+1]));
+										cwd[strlen(cwd)] = '\0';
+									}else if(argv[index][1]=='o'){
+										salida = (char *)malloc(strlen(argv[index+1])+1);
+										memcpy(salida,argv[index+1],strlen(argv[index+1]));
+										salida[strlen(salida)] = '\0';
+									}else{
+										fprintf(stderr, "Uso esperado : %s  [-h] |  [-n i] [-d directorio] [-o salida ]\n", argv[0]);
+										return 1;
+									}
+								}else{
+									fprintf(stderr, "Uso esperado : %s  [-h] |  [-n i] [-d directorio] [-o salida ]\n", argv[0]);
+									return 1;
+								}
+							}
+						}	
+					}
+				}
+			}
+		}
 	}
-    else{
-        perror("getcwd() error");
-    	return 0;
-	}
+	/* Revisar los directorios del master para agregarlos a la cola*/
 	Buscar(cwd);
 	printf("La Cola es: \n\n");
 	Print();
